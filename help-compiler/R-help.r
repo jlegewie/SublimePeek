66577f3cd9a1b471efa6b3d2a6b0efb95364f97b
Joscha Legewie <jpl2136@columbia.edu>
2012-04-12 11:24:46 -0400

    copy style file (R.css) to folder with R help files

--- help-compiler/R-help.r
+++ help-compiler/R-help.r
@@ -82,6 +82,9 @@ for(pkg in pkg.loaded) {
     cat(" done!\n")
 }
 
+# copy style file (R.css) to folder with R help files
+file.copy(paste(loc,"SublimePeek/help-compiler/R.css",sep=""),paste(loc,"SublimePeek-R-help/R.css",sep=""))
+
 # end of file ggplot
 cat("\n\nHelp files from ",n.f," functions in ", n.p," packages saved to JSON.\n")
 
